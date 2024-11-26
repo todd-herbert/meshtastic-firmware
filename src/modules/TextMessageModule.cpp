3fd2819387e4eb87d15c07073b4381cd364754ae
Todd Herbert <herbert.todd@gmail.com>
2024-11-26 19:00:34 +1300

    Echo received DMs back to sender

--- src/modules/TextMessageModule.cpp
+++ src/modules/TextMessageModule.cpp
@@ -6,6 +6,29 @@
 #include "configuration.h"
 TextMessageModule *textMessageModule;
 
+static void sendText(NodeNum dest, ChannelIndex channel, const char *message, bool wantReplies)
+{
+    meshtastic_MeshPacket *p = router->allocForSending();
+    p->decoded.portnum = meshtastic_PortNum_TEXT_MESSAGE_APP;
+
+    p->to = dest;
+    p->channel = channel;
+    p->want_ack = true;
+    p->decoded.payload.size = strlen(message);
+    memcpy(p->decoded.payload.bytes, message, p->decoded.payload.size);
+    if (moduleConfig.canned_message.send_bell && p->decoded.payload.size < meshtastic_Constants_DATA_PAYLOAD_LEN) {
+        p->decoded.payload.bytes[p->decoded.payload.size] = 7;        // Bell character
+        p->decoded.payload.bytes[p->decoded.payload.size + 1] = '\0'; // Bell character
+        p->decoded.payload.size++;
+    }
+
+    LOG_INFO("Send reply id=%d, dest=%x, msg=%.*s", p->id, p->to, p->decoded.payload.size, p->decoded.payload.bytes);
+
+    service->sendToMesh(
+        p, RX_SRC_LOCAL,
+        true); // send to mesh, cc to phone. Even if there's no phone connected, this stores the message to match ACKs
+}
+
 ProcessMessage TextMessageModule::handleReceived(const meshtastic_MeshPacket &mp)
 {
 #ifdef DEBUG_PORT
@@ -19,6 +42,12 @@ ProcessMessage TextMessageModule::handleReceived(const meshtastic_MeshPacket &mp
 
     powerFSM.trigger(EVENT_RECEIVED_MSG);
     notifyObservers(&mp);
+
+    if (mp.to != NODENUM_BROADCAST && mp.to != 0) {
+        std::string reply = "Got Message: ";
+        reply += (char *)mp.decoded.payload.bytes;
+        sendText(mp.from, 0, reply.c_str(), false);
+    }
 
     return ProcessMessage::CONTINUE; // Let others look at this message also if they want
 }

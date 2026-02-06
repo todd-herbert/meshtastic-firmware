abb7411f02a1c85be5df0d0ce3252c57b8b2de9f
Todd Herbert <herbert.todd@gmail.com>
2026-02-07 02:33:56 +1300

    Temporarily revert "Inkhud battery icon improvements. (#9513)" for efficiency issues.
    This reverts commit b7db22055d3a964d188ccbffce4c4ab3a3df6f0e.
    
    This change may be reimplemented in future.

--- src/graphics/niche/InkHUD/Applets/System/BatteryIcon/BatteryIconApplet.cpp
+++ src/graphics/niche/InkHUD/Applets/System/BatteryIcon/BatteryIconApplet.cpp
@@ -46,27 +46,37 @@ int InkHUD::BatteryIconApplet::onPowerStatusUpdate(const meshtastic::Status *sta
 
 void InkHUD::BatteryIconApplet::onRender()
 {
-    // Clear the region beneath the tile, including the border
+    // Fill entire tile
+    // - size of icon controlled by size of tile
+    int16_t l = 0;
+    int16_t t = 0;
+    uint16_t w = width();
+    int16_t h = height();
+
+    // Clear the region beneath the tile
     // Most applets are drawing onto an empty frame buffer and don't need to do this
     // We do need to do this with the battery though, as it is an "overlay"
-    fillRect(0, 0, width(), height(), WHITE);
+    fillRect(l, t, w, h, WHITE);
+
+    // Vertical centerline
+    const int16_t m = t + (h / 2);
 
     // =====================
     // Draw battery outline
     // =====================
 
     // Positive terminal "bump"
+    const int16_t &bumpL = l;
+    const uint16_t bumpH = h / 2;
+    const int16_t bumpT = m - (bumpH / 2);
     constexpr uint16_t bumpW = 2;
-    const int16_t &bumpL = 1;
-    const uint16_t bumpH = (height() - 2) / 2;
-    const int16_t bumpT = (1 + ((height() - 2) / 2)) - (bumpH / 2);
     fillRect(bumpL, bumpT, bumpW, bumpH, BLACK);
 
     // Main body of battery
-    const int16_t bodyL = 1 + bumpW;
-    const int16_t &bodyT = 1;
-    const int16_t &bodyH = height() - 2;         // Handle top/bottom padding
-    const int16_t bodyW = (width() - 1) - bumpW; // Handle 1px left pad
+    const int16_t bodyL = bumpL + bumpW;
+    const int16_t &bodyT = t;
+    const int16_t &bodyH = h;
+    const int16_t bodyW = w - bumpW;
     drawRect(bodyL, bodyT, bodyW, bodyH, BLACK);
 
     // Erase join between bump and body
@@ -77,13 +87,12 @@ void InkHUD::BatteryIconApplet::onRender()
     // ===================
 
     constexpr int16_t slicePad = 2;
-    int16_t sliceL = bodyL + slicePad;
+    const int16_t sliceL = bodyL + slicePad;
     const int16_t sliceT = bodyT + slicePad;
     const uint16_t sliceH = bodyH - (slicePad * 2);
     uint16_t sliceW = bodyW - (slicePad * 2);
 
-    sliceW = (sliceW * socRounded) / 100;          // Apply percentage
-    sliceL += ((bodyW - (slicePad * 2)) - sliceW); // Shift slice to the battery's negative terminal, correcting drain direction
+    sliceW = (sliceW * socRounded) / 100; // Apply percentage
 
     hatchRegion(sliceL, sliceT, sliceW, sliceH, 2, BLACK);
     drawRect(sliceL, sliceT, sliceW, sliceH, BLACK);
